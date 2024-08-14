e7ad924ff8f2b24136fc1e840844d10b5719fc9d
Corey Derochie <corey.derochie@amd.com>
2024-08-14 11:47:42 -0500

    Merged static-mscclpp

--- src/include/mscclpp/mscclpp_nccl.h
+++ src/include/mscclpp/mscclpp_nccl.h
@@ -14,27 +14,27 @@ typedef struct mscclpp_ncclComm* mscclpp_ncclComm_t;
 
 typedef struct { char internal[NCCL_UNIQUE_ID_BYTES]; } mscclpp_ncclUniqueId;
 
-bool mscclpp_init();
-
 /* A ncclUniqueId and a mscclpp_ncclUniqueId will always be created together and used alternatively. This maps between them. */
 extern std::unordered_map<ncclUniqueId, mscclpp_ncclUniqueId> mscclpp_uniqueIdMap;
 
-/* See ncclGetUniqueId. */
-extern ncclResult_t  (*mscclpp_ncclGetUniqueId)(mscclpp_ncclUniqueId* uniqueId);
+extern "C" {
+  /* See ncclGetUniqueId. */
+  ncclResult_t  mscclpp_ncclGetUniqueId(mscclpp_ncclUniqueId* uniqueId);
 
-/* See ncclCommInitRank. */
-extern ncclResult_t  (*mscclpp_ncclCommInitRank)(mscclpp_ncclComm_t* comm, int nranks, mscclpp_ncclUniqueId commId, int rank);
+  /* See ncclCommInitRank. */
+  ncclResult_t  mscclpp_ncclCommInitRank(mscclpp_ncclComm_t* comm, int nranks, mscclpp_ncclUniqueId commId, int rank);
 
-/* See ncclCommDestroy. */
-extern ncclResult_t  (*mscclpp_ncclCommDestroy)(mscclpp_ncclComm_t comm);
+  /* See ncclCommDestroy. */
+  ncclResult_t  mscclpp_ncclCommDestroy(mscclpp_ncclComm_t comm);
 
-/* See ncclAllReduce. */
-extern ncclResult_t  (*mscclpp_ncclAllReduce)(const void* sendbuff, void* recvbuff, size_t count,
-    ncclDataType_t datatype, ncclRedOp_t op, mscclpp_ncclComm_t comm, hipStream_t stream);
+  /* See ncclAllReduce. */
+  ncclResult_t  mscclpp_ncclAllReduce(const void* sendbuff, void* recvbuff, size_t count,
+      ncclDataType_t datatype, ncclRedOp_t op, mscclpp_ncclComm_t comm, hipStream_t stream);
 
-/* See ncclAllGather. */
-extern ncclResult_t  (*mscclpp_ncclAllGather)(const void* sendbuff, void* recvbuff, size_t sendcount,
-    ncclDataType_t datatype, mscclpp_ncclComm_t comm, hipStream_t stream);
+  /* See ncclAllGather. */
+  ncclResult_t  mscclpp_ncclAllGather(const void* sendbuff, void* recvbuff, size_t sendcount,
+      ncclDataType_t datatype, mscclpp_ncclComm_t comm, hipStream_t stream);
+}
 
 namespace std {
   template <>
