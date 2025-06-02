07e4c3763b29e00595616e339e8f3c7de875622e
Nusrat Islam <Nusrat.Islam@amd.com>
2025-06-02 14:02:50 -0500

    msccl: enable multi-node msccl direct allreduce

--- src/include/msccl/msccl_struct.h
+++ src/include/msccl/msccl_struct.h
@@ -13,7 +13,7 @@
 #include "device.h"
 #include "msccl/msccl_scheduler.h"
 
-#define MSCCL_MAX_NUM_STEPS 64
+#define MSCCL_MAX_NUM_STEPS 256
 #define MSCCL_MAX_NUM_THREAD_BLOCKS_PER_CHANNEL 32
 #define MSCCL_MAX_NUM_THREAD_BLOCKS 64
 #define MSCCL_MAX_COUNT 72 // max concurrent number of msccl chunk transmission
